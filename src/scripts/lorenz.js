a009d39c5daf057eabe3af9c893a5998fef89a4c
Clive Chan <cc@clive.io>
2016-08-04 01:31:34 -0400

    Got simulation details on mouseover working, z-index magic

--- src/scripts/lorenz.js
+++ src/scripts/lorenz.js
@@ -1,6 +1,6 @@
 // (Adapted from http://wow.techbrood.com/static/20151028/2960.html)
 $(function(){
-  $("#splash-details").text("Lorenz Strange Attractor");
+  $("#simulation-name").text("Lorenz Strange Attractor");
   $("main").click(function(e){e.stopPropagation();});
 
   //I've included as many comments as possible for the variables associated with Lorenz Mathamatics so you can edit and play.  The rest of the script should be self - explanatory, if not, you can refer to the D3.js docs.
