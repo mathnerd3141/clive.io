a74ffdbe5627dfca0ebf0dd1e2e18f97fc121229
Clive Chan <cc@clive.io>
2019-05-11 13:51:00 -0700

    build: fix browsersyncing, improve parallelism, error handling, add clean target

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const gulpSequence = require('gulp-sequence').use(gulp);
-const pump = require('pump');
-const sourceMaps = require('gulp-sourcemaps');
 const gulpif = require('gulp-if');
 
 const PROD = !!process.env.PROD;
@@ -13,38 +10,25 @@ const PATHS = {
   sass: "src/styles/**/*.{scss,sass}",
   ts: "src/scripts/**/*.ts",
   static: "static/**/*.*",
-  dist: "dist"
+  dist: "dist",
+  build: "build"
 };
 
-function swallowError(desc){
-  return function(e){
-    console.error("Error in " + desc + ":" + e.toString());
-    if(!PROD)
-      this.emit('end');
-  }
-}
-
-const ts = require('gulp-typescript');
-const Hjson = require('hjson');
-const fs = require('fs');
-const tsConfig = Hjson.parse(fs.readFileSync('./tsconfig.json').toString());
-const tsProject = ts.createProject(tsConfig.compilerOptions);
-gulp.task('ts', function(cb) {
-  const uglify = require('gulp-uglify');
+gulp.task('ts', function() {
   const concat = require('gulp-concat');
-  pump([
-      gulp.src(PATHS.ts),
-      gulpif(!PROD, sourceMaps.init()),
-      tsProject(),
-      concat('script.js'),
-      uglify(),
-      gulpif(!PROD, sourceMaps.write()),
-      gulp.dest(PATHS.dist),
-      gulpif(!PROD, browserSync.stream())
-    ],
-    cb
-  )
-  .on('error', swallowError('ts'));
+  const sourceMaps = require('gulp-sourcemaps');
+  const ts = require('gulp-typescript');
+  const Hjson = require('hjson');
+  const fs = require('fs');
+  const tsConfig = Hjson.parse(fs.readFileSync('./tsconfig.json').toString());
+  const tsProject = ts.createProject(tsConfig.compilerOptions);
+
+  return gulp.src(PATHS.ts)
+    .pipe(gulpif(!PROD, sourceMaps.init()))
+    .pipe(tsProject())
+    .pipe(concat('script.js'))
+    .pipe(gulpif(!PROD, sourceMaps.write()))
+    .pipe(gulp.dest(PATHS.build));
 });
 
 gulp.task('pug', function() {
@@ -52,7 +36,7 @@ gulp.task('pug', function() {
 
   return gulp.src(PATHS.pugsrc)
     .pipe(pug())
-    .on('error', swallowError('pug'))
+    .pipe(gulp.dest(PATHS.build))
     .pipe(gulp.dest(PATHS.dist));
 });
 
@@ -61,35 +45,52 @@ gulp.task('cache-bust', function() {
   return gulp.src(PATHS.dist + '/**/*.html')
     .pipe(cachebust())
     .pipe(gulp.dest(PATHS.dist))
-    .pipe(gulpif(!PROD, browserSync.stream()));
-})
+    .pipe(gulpif(!PROD, browserSync.stream({once: true})));
+});
 
 gulp.task('sass', function() {
   const sass = require('gulp-sass');
   const autoprefixer = require('gulp-autoprefixer');
   const concat = require('gulp-concat');
-  const purify = require('gulp-purifycss');
-  const uglify = require('gulp-uglifycss');
+  const sourceMaps = require('gulp-sourcemaps');
 
   return gulp.src(PATHS.sass)
     .pipe(gulpif(!PROD, sourceMaps.init()))
-    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle:'compressed'}))
     .pipe(autoprefixer())
     .pipe(concat('style.css'))
-    .pipe(purify(['./dist/**.html', './dist/**.js']))
-    .pipe(uglify())
     .pipe(gulpif(!PROD, sourceMaps.write()))
+    .pipe(gulp.dest(PATHS.dist));
+});
+
+gulp.task('purifycss', function() {
+  const purify = require('gulp-purifycss');
+  const uglify = require('gulp-uglifycss');
+
+  return gulp.src(PATHS.dist + '/style.css')
+    .pipe(purify(['./build/**.html', './dist/**.js']))
+    .pipe(uglify())
     .pipe(gulp.dest(PATHS.dist))
-    .pipe(gulpif(!PROD, browserSync.stream()));
+    .pipe(gulpif(!PROD, browserSync.stream({once: true})));
+});
+
+gulp.task('uglifyjs', function(){
+  const uglify = require('gulp-uglify');
+  return gulp.src(PATHS.build + '/script.js')
+      .pipe(uglify())
+      .pipe(gulp.dest(PATHS.dist))
+      .pipe(gulpif(!PROD, browserSync.stream({once: true})));
 });
 
 gulp.task('static', function() {
   return gulp.src(PATHS.static)
-    .pipe(gulp.dest(PATHS.dist))
-    .pipe(gulpif(!PROD, browserSync.stream()));
+    .pipe(gulp.dest(PATHS.dist));
 });
 
-gulp.task('build', gulp.series(gulp.parallel('ts', 'pug', 'static'), 'sass', 'cache-bust'));
+gulp.task('build', gulp.series(
+  gulp.parallel('ts', 'pug', 'static', 'sass'),
+  gulp.parallel('purifycss', 'cache-bust', 'uglifyjs')
+));
 
 gulp.task('serve-dev', gulp.series('build', function() {
   browserSync.init({
@@ -100,26 +101,18 @@ gulp.task('serve-dev', gulp.series('build', function() {
     logSnippet: false
   });
 
-  gulp.watch(PATHS.ts, function (event) {
-    gulpSequence('ts', 'cache-bust')(function (err) {
-      if (err) console.log(err)
-    })
-  });
-  gulp.watch(PATHS.pugwatch, function (event) {
-    gulpSequence('pug', 'cache-bust')(function (err) {
-      if (err) console.log(err)
-    })
-  });
-  gulp.watch(PATHS.sass, function (event) {
-    gulpSequence('sass', 'cache-bust')(function (err) {
-      if (err) console.log(err)
-    })
-  });
-  gulp.watch(PATHS.static, function (event) {
-    gulpSequence('static', 'cache-bust')(function (err) {
-      if (err) console.log(err)
-    })
-  });
+  gulp.watch(PATHS.ts, gulp.series('ts', gulp.parallel('purifycss', 'cache-bust', 'uglifyjs')));
+  gulp.watch(PATHS.pugwatch, gulp.series('pug', gulp.parallel('purifycss', 'cache-bust')));
+  gulp.watch(PATHS.sass, gulp.series('sass', gulp.parallel('purifycss', 'cache-bust')));
+  gulp.watch(PATHS.static, gulp.series('static', gulp.parallel('purifycss', 'cache-bust')));
 }));
+
+gulp.task('clean', function() {
+  const del = require('del');
+  return del([
+    'dist',
+    'build'
+  ]);
+});
 
 gulp.task('default', gulp.series('serve-dev'));
